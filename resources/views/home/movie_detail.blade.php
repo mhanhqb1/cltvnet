1560421d19c09881c13c5f6e92fa40e2b2d84408
AnhMH <anh.mh@moziasoft.com>
2022-08-08 21:50:46 +0700

    Change video player

--- resources/views/home/movie_detail.blade.php
+++ resources/views/home/movie_detail.blade.php
@@ -43,6 +43,26 @@ $cateName = implode(' - ', $cateName);
                 </div>
             </div>
         </div>
+        @if (empty($movie->is_series))
+        <div class="inner-box category-content" style="padding-bottom: 15px;">
+            <h2 class="title-2" style="color:#cd1d1f; font-weight:bold;"> <i class="fa fa-tags"></i>
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">Xem phim</font>
+                </font>
+            </h2>
+            <div class="faq-content">
+                <div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;" itemscope itemtype="https://schema.org/VideoObject">
+                    <meta itemprop="name" content="Cười Không Nhặt Được Mồm - Review Phim Thánh Bài 2 Châu Tinh Trì - Vua Phim #2" />
+                    <meta itemprop="description" content="Review Phim Thánh Bài 2 Châu Tinh Trì, đỗ thánh châu tinh trì phim thần bài phần 2<br />tom tat phim hai chau tinh tri than bai II<br />" />
+                    <meta itemprop="uploadDate" content="2022-08-02T13:28:32.000Z" />
+                    <meta itemprop="thumbnailUrl" content="https://s1.dmcdn.net/v/U7Qq61YwIqz58eo1b/x180" />
+                    <meta itemprop="duration" content="P748S" />
+                    <meta itemprop="embedUrl" content="https://geo.dailymotion.com/player/x9pog.html?video=x8cti4m" />
+                    <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden" frameborder="0" type="text/html" src="https://geo.dailymotion.com/player/x9pog.html?video=x8cti4m" width="100%" height="100%" allow="fullscreen; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+        @endif
         <div class="inner-box category-content" style="padding-bottom: 10px;">
             <h2 class="title-2" style="color:#cd1d1f; font-weight:bold;"> <i class="fa fa-tags"></i>
                 <font style="vertical-align: inherit;">
@@ -137,25 +157,6 @@ $cateName = implode(' - ', $cateName);
                             @endforeach
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        @else
-        <div class="inner-box category-content" style="padding-bottom: 15px;">
-            <h2 class="title-2" style="color:#cd1d1f; font-weight:bold;"> <i class="fa fa-tags"></i>
-                <font style="vertical-align: inherit;">
-                    <font style="vertical-align: inherit;">Xem phim</font>
-                </font>
-            </h2>
-            <div class="faq-content">
-                <div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;" itemscope itemtype="https://schema.org/VideoObject">
-                    <meta itemprop="name" content="Cười Không Nhặt Được Mồm - Review Phim Thánh Bài 2 Châu Tinh Trì - Vua Phim #2" />
-                    <meta itemprop="description" content="Review Phim Thánh Bài 2 Châu Tinh Trì, đỗ thánh châu tinh trì phim thần bài phần 2<br />tom tat phim hai chau tinh tri than bai II<br />" />
-                    <meta itemprop="uploadDate" content="2022-08-02T13:28:32.000Z" />
-                    <meta itemprop="thumbnailUrl" content="https://s1.dmcdn.net/v/U7Qq61YwIqz58eo1b/x180" />
-                    <meta itemprop="duration" content="P748S" />
-                    <meta itemprop="embedUrl" content="https://geo.dailymotion.com/player/x9pog.html?video=x8cti4m" />
-                    <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden" frameborder="0" type="text/html" src="https://geo.dailymotion.com/player/x9pog.html?video=x8cti4m" width="100%" height="100%" allow="fullscreen; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
