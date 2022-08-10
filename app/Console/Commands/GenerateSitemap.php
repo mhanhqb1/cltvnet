883da391050320f1c01377839b40d2089e4dc516
AnhMH <anh.mh@moziasoft.com>
2022-08-10 11:21:12 +0700

    Fix batch sitemap

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,9 +4,11 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 use App\Models\Movie;
 use App\Models\Cate;
 use App\Models\MovieVideo;
+use Carbon\Carbon;
 
 class GenerateSitemap extends Command
 {
@@ -33,12 +35,32 @@ class GenerateSitemap extends Command
     {
         // creates sitemap with all urls in your website
         $sitemap = Sitemap::create();
+
+        // Categories
         $sitemap = $sitemap->add(route('home'));
         $sitemap = $sitemap->add(route('home.new_movie'));
         $sitemap = $sitemap->add(route('home.anime'));
         $sitemap = $sitemap->add(route('home.not_series'));
         $sitemap = $sitemap->add(route('home.series'));
-        echo public_path('sitemap.xml'); die();
+        $cates = Cate::get();
+        foreach ($cates as $cate) {
+            $sitemap = $sitemap->add(route('home.cate.index', $cate->slug));
+        }
+
+        // Movies
+        $movies = Movie::orderBy('year', 'desc')->orderBy('id', 'desc')->get();
+        foreach ($movies as $v) {
+            $sitemap = $sitemap->add(URL::create(route('home.movie_detail', $v->slug))->setLastModificationDate(Carbon::parse($v->updated_at)));
+        }
+
+        // Video
+        $videos = MovieVideo::with('movie')->whereHas('movie', function($q) {
+            $q->where('movies.is_series', 1);
+        })->orderBy('id', 'desc')->get();
+        foreach ($videos as $v) {
+            $sitemap = $sitemap->add(URL::create(route('home.video_detail', ['movieSlug' => $v->movie->slug, 'videoSlug' => $v->slug]))->setLastModificationDate(Carbon::parse($v->updated_at)));
+        }
+
         $sitemap->writeToFile(public_path('sitemap.xml'));
     }
 }
