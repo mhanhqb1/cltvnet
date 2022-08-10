cbfb8d7aa98be3d511cab441cd7af75e55227729
AnhMH <anh.mh@moziasoft.com>
2022-08-10 17:23:33 +0700

    Fix related movies

--- resources/views/home/movie_detail.blade.php
+++ resources/views/home/movie_detail.blade.php
@@ -40,6 +40,7 @@ $cateName = implode(' - ', $cateName);
             </div>
         </div>
 
+        @if(!$relatedMovies->isEmpty())
         <div class="inner-box category-content" style="padding-bottom: 15px;">
             <h2 class="title-2" style="color:#cd1d1f; font-weight:bold;"> <i class="fa fa-tags"></i>
                 <font style="vertical-align: inherit;">
@@ -47,32 +48,32 @@ $cateName = implode(' - ', $cateName);
                 </font>
             </h2>
             <div class="faq-content">
-                @if(!$relatedMovies->isEmpty())
-                    <ul class="related-movies">
-                        @foreach($relatedMovies as $v)
-                        <li>
-                            <div class="movie-image">
-                                <a href="{{ route('home.movie_detail', $v->slug) }}" target="_blank" title="{{ $v->name }}">
-                                    <img src="{{ getImageUrl($v->image) }}" alt="{{ $v->name }}"/>
-                                </a>
-                            </div>
-                            <div class="movie-info">
-                                <h4><a href="{{ route('home.movie_detail', $v->slug) }}" target="_blank">{{ $v->name }}</a></h4>
-                                @if(!empty($v->cates))
-                                    <p>
-                                        <a href="{{ route('home.country.index', $v->country->slug) }}" title="Review phim {{ $v->country->name }}">{{ $v->country->name }}</a>
-                                        @foreach($v->cates as $c)
-                                        - <a href="{{ route('home.cate.index', $c->slug) }}" target="_blank" title="Review phim {{ $c->name }}">{{ $c->name }}</a>
-                                        @endforeach
-                                    </p>
-                                @endif
-                            </div>
-                        </li>
-                        @endforeach
-                    </ul>
-                @endif
+                <ul class="related-movies">
+                    @foreach($relatedMovies as $v)
+                    <li>
+                        <div class="movie-image">
+                            <a href="{{ route('home.movie_detail', $v->slug) }}" target="_blank" title="{{ $v->name }}">
+                                <img src="{{ getImageUrl($v->image) }}" alt="{{ $v->name }}"/>
+                            </a>
+                        </div>
+                        <div class="movie-info">
+                            <h4><a href="{{ route('home.movie_detail', $v->slug) }}" target="_blank">{{ $v->name }}</a></h4>
+                            @if(!empty($v->cates))
+                                <p>
+                                    <a href="{{ route('home.country.index', $v->country->slug) }}" title="Review phim {{ $v->country->name }}">{{ $v->country->name }}</a>
+                                    @foreach($v->cates as $c)
+                                    - <a href="{{ route('home.cate.index', $c->slug) }}" target="_blank" title="Review phim {{ $c->name }}">{{ $c->name }}</a>
+                                    @endforeach
+                                </p>
+                            @endif
+                        </div>
+                    </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
+        @endif
+
         @endif
         <div class="inner-box category-content" style="padding-bottom:20px;">
             <h1 class="title-2" style="color:#cd1d1f; font-weight:bold;"> <i class="fa fa-video-camera"></i>
