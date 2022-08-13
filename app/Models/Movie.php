84bb1296b016b9e4c8eda2019d415532674a818d
AnhMH <anh.mh@moziasoft.com>
2022-08-13 10:51:10 +0700

    Fix batch daily

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -77,7 +77,7 @@ class Movie extends Model
     }
 
     public static function dailyPlayListCrawler($limit = 20, $checkTime = true) {
-        $time = date('Y-m-d H:i:s', time() - 1*3600);
+        $time = date('Y-m-d H:i:s', time() - 1*60*60);
         $movies = Movie::whereNotNull('daily_playlist_id')
             ->where('daily_playlist_id', '!=', '');
         if ($checkTime) {
