b137bf6baf252a6d72399be5aee444fac8c41b19
AnhMH <anh.mh@moziasoft.com>
2022-08-15 16:22:31 +0700

    Fix video display

--- resources/views/layouts/video.blade.php
+++ resources/views/layouts/video.blade.php
@@ -4,7 +4,7 @@ $name = !empty($item->movie->is_series) ? $item->movie->name.' - '.$item->name :
 $url = empty($item->movie->is_series) ? route('home.movie_detail', $item->movie->slug) : route('home.video_detail', ['movieSlug' => $item->movie->slug, 'videoSlug' => $item->slug]);
 ?>
 <div class="col-xs-6 col-sm-2 movie-item">
-    <span class="movie-label">{{ !empty($item->movie->is_series) ? $item->position : $item->movie->year }}</span>
+    <span class="movie-label">{{ !empty($item->movie->is_series) ? $item->name : $item->movie->year }}</span>
     <a href="{{ $url }}">
         <img src="{{ asset('/images/blank.png') }}" data-src="{!! $imageUrl !!}" alt="{{ $name }}" style="width:100%; height:233px;" class="lazyload" /><br />
         <center>
