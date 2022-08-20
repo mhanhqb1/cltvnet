a39619b611cb01809764f41e1f1c2c1850b2c851
AnhMH <anh.mh@moziasoft.com>
2022-08-20 11:26:22 +0700

    fix ads

--- resources/views/layouts/detail_ads.blade.php
+++ resources/views/layouts/detail_ads.blade.php
@@ -13,7 +13,7 @@
             </script>
         </div> -->
         <center>
-        <div class="col-sm-12">
+        <!-- <div class="col-sm-12">
             <script type="text/javascript">
                 atOptions = {
                     'key': '5c69f71391f5cbcd667f2d7e489f8ca9',
@@ -24,13 +24,13 @@
                 };
                 document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/5c69f71391f5cbcd667f2d7e489f8ca9/invoke.js"></scr' + 'ipt>');
             </script>
-        </div>
+        </div> -->
         </center>
     </div>
 </div>
 <div class="row">
-    <div class="col-sm-12">
+    <!-- <div class="col-sm-12">
         <script async="async" data-cfasync="false" src="//pl17602823.highperformancegate.com/4d6ca70c3586b0ea4842fdf40ea18b03/invoke.js"></script>
         <div id="container-4d6ca70c3586b0ea4842fdf40ea18b03"></div>
-    </div>
+    </div> -->
 </div>
