3b81b24498b1123a0a649827c546600ffb883f0d
AnhMH <anh.mh@moziasoft.com>
2022-11-23 22:25:42 +0700

    Add welcome page

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
