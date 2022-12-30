3f1f9e19ea8277800ca6bde769161c4812dd545e
AnhMH <anh.mh@moziasoft.com>
2022-12-30 16:50:07 +0700

    Add contact send mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Contact;
+use App\Models\Setting;
+use App\Mail\SendMail;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -20,12 +23,19 @@ class ContactController extends Controller
     }
     public function save(Request $request)
     {
-        $contact = new Contact();
-        $contact->name = !empty($request->name) ? $request->name : '';
-        $contact->phone = !empty($request->phone) ? $request->phone : '';
-        $contact->email = !empty($request->email) ? $request->email : '';
-        $contact->message = !empty($request->message) ? $request->message : '';
-        $contact->save();
+        $setting = Setting::where('name', 'web_email')->first();
+        $name = !empty($request->name) ? $request->name : '';
+        $phone = !empty($request->phone) ? $request->phone : '';
+        $email = !empty($request->email) ? $request->email : '';
+        $message = !empty($request->message) ? $request->message : '';
+        $mailData = [
+            'name' => $name,
+            'phone' => $phone,
+            'email' => $email,
+            'message' => $message
+        ];
+        Contact::create($mailData);
+        Mail::to($setting->value)->send(new SendMail($mailData));
         echo 'success'; exit();
         return redirect()->route('front.contact.index')->with('success', 'Dữ liệu đã được cập nhật thành công');
     }
