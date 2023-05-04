3c373d2ca8ce21e2c0e66be8fdbfe4df57f77e1a
AnhMH <anh.mh@moziasoft.com>
2023-05-04 08:28:10 +0700

    aa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->command('inspire')->hourly();// a
         $schedule->command('generate:sitemap')->daily();
         // $schedule->command('CaLaTV:DailyPlaylistCrawler')->everyThirtyMinutes();
         $schedule->command('CaLaTV:DailymotionCrawler')->everySixHours();
