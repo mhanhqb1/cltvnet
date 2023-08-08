6ce6df617bba6d296122e68cc820b8c3bea1e8c4
AnhMH <anh.mh@moziasoft.com>
2023-08-08 17:15:34 +0700

    feat: add text editor

--- resources/views/components/text-editor.blade.php
+++ resources/views/components/text-editor.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $label }}</label>
-    <textarea id="{{ $name }}" name="{{ $name }}" class="form-control text-editor" rows="8">{{ $value }}</textarea>
+    <textarea id="summernote" name="{{ $name }}" class="form-control text-editor" rows="8">{!! $value !!}</textarea>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,9 +93,11 @@
 </div>
 
 <script src="{{ mix('js/app.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote.min.js" integrity="sha512-6rE6Bx6fCBpRXG/FWpQmvguMWDLWMQjPycXMr35Zx/HRD9nwySZswkkLksgyQcvrpYMx0FELLJVBvWFtubZhDQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
     $(document).ready(function() {
         $('.select2').select2();
+        $('#summernote').summernote();
     });
 </script>
 @stack('third_party_scripts')
