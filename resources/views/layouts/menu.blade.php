36e5cf9779ccbe2990c854f26a9b9e0e2927bd6f
AnhMH <anh.mh@moziasoft.com>
2023-08-14 21:46:59 +0700

    feat: add menu icon

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -7,31 +7,31 @@
 </li>
 <li class="nav-item">
     <a href="{{ route('admin.nutritions.index') }}" class="nav-link {{ request()->route()->named('admin.nutritions.*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-heartbeat"></i>
         <p>{{ __('nutrition_menu') }}</p>
     </a>
 </li>
 <li class="nav-item">
     <a href="{{ route('admin.cates.index') }}" class="nav-link {{ request()->route()->named('admin.cates.*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-sitemap"></i>
         <p>{{ __('cate_menu') }}</p>
     </a>
 </li>
 <li class="nav-item">
     <a href="{{ route('admin.ingredients.index') }}" class="nav-link {{ request()->route()->named('admin.ingredients.*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-balance-scale"></i>
         <p>{{ __('ingredient_menu') }}</p>
     </a>
 </li>
 <li class="nav-item">
     <a href="{{ route('admin.foods.index') }}" class="nav-link {{ request()->route()->named('admin.foods.*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-cube"></i>
         <p>{{ __('food_menu') }}</p>
     </a>
 </li>
 <li class="nav-item">
     <a href="{{ route('admin.menus.index') }}" class="nav-link {{ request()->route()->named('admin.menus.*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-book"></i>
         <p>{{ __('menu_menu') }}</p>
     </a>
 </li>
@@ -46,25 +46,25 @@
     <ul class="nav nav-treeview">
         <li class="nav-item">
             <a href="{{ route('admin.cala.products.index') }}" class="nav-link {{ request()->route()->named('admin.cala.products.*') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-birthday-cake"></i>
                 <p>{{ __('product_menu') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('admin.cala.transporters.index') }}" class="nav-link {{ request()->route()->named('admin.cala.transporters.*') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-car"></i>
                 <p>{{ __('transporter_menu') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('admin.cala.customers.index') }}" class="nav-link {{ request()->route()->named('admin.cala.customers.*') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-user"></i>
                 <p>{{ __('customer_menu') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('admin.cala.orders.index') }}" class="nav-link {{ request()->route()->named('admin.cala.orders.*') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-cart-arrow-down"></i>
                 <p>{{ __('order_menu') }}</p>
             </a>
         </li>
