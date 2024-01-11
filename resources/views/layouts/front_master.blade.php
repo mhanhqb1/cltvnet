b24b2413a45f816fd41fd9228bc436b1219b6237
AnhMH <anh.mh@moziasoft.com>
2024-01-11 10:52:53 +0700

    remove ads

--- resources/views/layouts/front_master.blade.php
+++ resources/views/layouts/front_master.blade.php
@@ -123,48 +123,6 @@ $pageImage = !empty($pageImage) ? $pageImage : asset('images/banner.jpg');
                     Welcome to <strong>{{ $appName }}</strong>
                 </a>
             </div>
-            @if(env('APP_ENV') != 'local')
-            <div class="container">
-                <center>
-                <div class="col-sm-12">
-                    <script type="text/javascript">
-                        atOptions = {
-                            'key': '4d5af8e4f94a1a3fbbac9eeabdbe9a52',
-                            'format': 'iframe',
-                            'height': 90,
-                            'width': 728,
-                            'params': {}
-                        };
-                        document.write('<scr' + 'ipt type="text/javascript" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/4d5af8e4f94a1a3fbbac9eeabdbe9a52/invoke.js"></scr' + 'ipt>');
-                    </script>
-                </div>
-                <div class="col-sm-12">
-                    <script type="text/javascript">
-                        atOptions = {
-                            'key': 'b885faff3de006cdce1d13342d723a57',
-                            'format': 'iframe',
-                            'height': 60,
-                            'width': 468,
-                            'params': {}
-                        };
-                        document.write('<scr' + 'ipt type="text/javascript" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/b885faff3de006cdce1d13342d723a57/invoke.js"></scr' + 'ipt>');
-                    </script>
-                </div>
-                <div class="col-sm-12">
-                    <script type="text/javascript">
-                        atOptions = {
-                            'key': '2beb75141c9acf110403bc7a2eada0f5',
-                            'format': 'iframe',
-                            'height': 50,
-                            'width': 320,
-                            'params': {}
-                        };
-                        document.write('<scr' + 'ipt type="text/javascript" data-cfasync="false" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/2beb75141c9acf110403bc7a2eada0f5/invoke.js"></scr' + 'ipt>');
-                    </script>
-                </div>
-                </center>
-            </div>
-            @endif
             <div class="container">
                 @yield('content')
             </div>
