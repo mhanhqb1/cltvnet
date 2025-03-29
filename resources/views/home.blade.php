800e443f4d37ce35efef2900afbfb7dd0345c944
AnhMH <anh.mh@moziasoft.com>
2025-03-29 10:46:58 +0700

    Fix homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
                                     @endforeach
                                 @endif
                             </div>
-                            <center><a class="btn btn-block btn-border btn-post btn-danger" href="{{ route('home.new_movie') }}" target="_blank">VER TODAS</a></center>
+                            <center><a class="btn btn-block btn-border btn-post btn-danger" href="{{ route('home.series') }}" target="_blank">VER TODAS</a></center>
                         </fieldset>
                     </form>
                 </div>
