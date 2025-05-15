1a3d500f74d0b16c89f91dc726a9c8ba826b0723
AnhMH <anh.mh@moziasoft.com>
2025-05-15 11:01:38 +0700

    Fix ads

--- resources/views/layouts/ads/mid_ads.blade.php
+++ resources/views/layouts/ads/mid_ads.blade.php
@@ -2,8 +2,10 @@
 $showAds = env('SHOW_ADS');
 ?>
 @if($showAds)
-<div class="ad-pc" style="margin: 10px auto; width: fit-content; display:flex; gap: 10px;">
+<div class="ad-pc" style="margin: 10px auto; width: fit-content;">
     <ins class="eas6a97888e2" data-zoneid="5611518"></ins>
+</div>
+<div class="ad-pc" style="margin: 10px auto; width: fit-content;">
     <ins class="eas6a97888e2" data-zoneid="5611508"></ins>
 </div>
 <div class="ad-mobile" style="margin: 10px auto; width: fit-content;">
