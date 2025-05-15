cbef34c0ec27b43ac2f6409b0a84bf80793c651a
AnhMH <anh.mh@moziasoft.com>
2025-05-15 11:04:25 +0700

    Fix mobile ads

--- resources/views/layouts/ads/mid_ads.blade.php
+++ resources/views/layouts/ads/mid_ads.blade.php
@@ -9,5 +9,9 @@ $showAds = env('SHOW_ADS');
     <ins class="eas6a97888e2" data-zoneid="5611508"></ins>
 </div>
 <div class="ad-mobile" style="margin: 10px auto; width: fit-content;">
+     <ins class="eas6a97888e10" data-zoneid="5611520"></ins>
+</div>
+<div class="ad-mobile" style="margin: 10px auto; width: fit-content;">
+    <ins class="eas6a97888e10" data-zoneid="5611524"></ins>
 </div>
 @endif
