08f577a3111cc3e79d3c1c8eccc41dc3d156a540
AnhMH <anh.mh@moziasoft.com>
2025-05-15 14:23:57 +0700

    Add ads

---
+++ resources/views/layouts/ads/instant_message.blade.php
@@ -0,0 +1,6 @@
+<?php
+$showAds = env('SHOW_ADS');
+?>
+@if($showAds)
+<ins class="eas6a97888e6" data-zoneid="5611710"></ins>
+@endif
--- resources/views/layouts/front_master.blade.php
+++ resources/views/layouts/front_master.blade.php
@@ -283,6 +283,7 @@ $showAds = env('SHOW_ADS');
                 </center>
             </div>
         </div>
+        @include('layouts.ads.instant_message')
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="{{ asset('/js/scripts.js?'.$cacheVersion) }}"></script>
