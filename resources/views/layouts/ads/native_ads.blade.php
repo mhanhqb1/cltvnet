6b4db9c6e1f47f191f789847c70e1d879b749403
AnhMH <anh.mh@moziasoft.com>
2025-05-15 11:11:48 +0700

    Fix ads

--- resources/views/layouts/ads/native_ads.blade.php
+++ resources/views/layouts/ads/native_ads.blade.php
@@ -2,7 +2,7 @@
 $showAds = env('SHOW_ADS');
 ?>
 @if($showAds)
-<div style="margin: 10px auto; width: fit-content;">
+<!-- <div style="margin: 10px auto; width: fit-content;"> -->
     <ins class="eas6a97888e20" data-zoneid="5611528"></ins>
-</div>
+<!-- </div> -->
 @endif
