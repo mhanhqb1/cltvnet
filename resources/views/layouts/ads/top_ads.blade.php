95cf1ce56bd38c0a508dbdadb7f0813c9c377431
AnhMH <anh.mh@moziasoft.com>
2025-05-18 09:43:35 +0700

    Fix ads

--- resources/views/layouts/ads/top_ads.blade.php
+++ resources/views/layouts/ads/top_ads.blade.php
@@ -3,9 +3,13 @@ $showAds = env('SHOW_ADS');
 ?>
 @if($showAds)
 <div class="ad-pc" style="margin: 10px auto; width: fit-content;">
-    <ins class="eas6a97888e2" data-zoneid="5611456"></ins>
+    <!-- <ins class="eas6a97888e2" data-zoneid="5611456"></ins> -->
+    <script type="text/javascript" src="https://udbaa.com/bnr.php?section=General&pub=771288&format=728x90&ga=g"></script>
+    <noscript><a href="https://yllix.com/publishers/771288" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/728x90.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript>
 </div>
 <div class="ad-mobile" style="margin: 10px auto; width: fit-content;">
-    <ins class="eas6a97888e10" data-zoneid="5611458"></ins>
+    <!-- <ins class="eas6a97888e10" data-zoneid="5611458"></ins> -->
+    <script type="text/javascript" src="https://udbaa.com/bnr.php?section=General&pub=771288&format=300x50&ga=g"></script>
+    <noscript><a href="https://yllix.com/publishers/771288" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub_zn9ugf.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript>
 </div>
 @endif
